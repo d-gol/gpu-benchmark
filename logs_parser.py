0dc7ab3ce58d08617c907bc7a83acd661de37823
Dejan Golubovic <dejan.golubovic.27@gmail.com>
2020-09-11 12:17:35 +0200

    parser

--- logs_parser.py
+++ logs_parser.py
@@ -1,23 +1,18 @@
 import subprocess
 
 log_pods = subprocess.check_output(['kubectl', 'get', 'pods'])
-pods_lines = log_pods.decode('UTF-8').split('\n')
+pods_lines = log_pods.decode('UTF-8').split()
 
 pod_template_name = 'dejan-tfjob-'
 pod_names = []
 for line in pods_lines:
-    if line.startswith(pod_template_name):
-        pod_names.append(line.split('\t')[0])
-        
-print('pods:')
-for pod in pod_names:
-    print(pod)
+    if line.startswith(pod_template_name) and 'worker' in line:
+        pod_names.append(line.split('\s')[0])
 
-res = subprocess.check_output(['kubectl', 'logs', 'dejan-tfjob-resnet50-256-worker-0'])
+res = subprocess.check_output(['kubectl', 'logs', pod_name])
 
 lines = res.decode('UTF-8').split('\n')
-for line in lines:
-    print(line)
+for line in lines: 
     if '1\timages/sec' in line:
         line_split = line.split('\s')
         start_time = line_split[0].split('|')[1]
