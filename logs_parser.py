245a76c91fed4d4af434a565a7378e546675a4a6
Dejan Golubovic <dejan.golubovic.27@gmail.com>
2020-09-11 12:12:27 +0200

    Update logs_parser.py

--- logs_parser.py
+++ logs_parser.py
@@ -1,5 +1,18 @@
 import subprocess
 
+log_pods = subprocess.check_output(['kubectl', 'get', 'pods'])
+pods_lines = log_pods.decode('UTF-8').split('\n')
+
+pod_template_name = 'dejan-tfjob-'
+pod_names = []
+for line in pods_lines:
+    if line.startswith(pod_template_name):
+        pod_names.append(line.split('\t')[0])
+        
+print('pods:')
+for pod in pod_names:
+    print(pod)
+
 res = subprocess.check_output(['kubectl', 'logs', 'dejan-tfjob-resnet50-256-worker-0'])
 
 lines = res.decode('UTF-8').split('\n')
