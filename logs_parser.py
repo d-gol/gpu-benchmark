79effc5788dd4bfb0d195349431f1d5cd1014675
Dejan Golubovic <dejan.golubovic.27@gmail.com>
2020-09-11 12:19:04 +0200

    Update logs_parser.py

--- logs_parser.py
+++ logs_parser.py
@@ -9,15 +9,16 @@ for line in pods_lines:
     if line.startswith(pod_template_name) and 'worker' in line:
         pod_names.append(line.split('\s')[0])
 
-res = subprocess.check_output(['kubectl', 'logs', pod_name])
+for pod_name in pod_names:
+    res = subprocess.check_output(['kubectl', 'logs', pod_name])
 
-lines = res.decode('UTF-8').split('\n')
-for line in lines: 
-    if '1\timages/sec' in line:
-        line_split = line.split('\s')
-        start_time = line_split[0].split('|')[1]
-    elif 'total images/sec' in line:
-        line_split = line.split('\s')
-        end_time = line_split[0].split('|')[1]
+    lines = res.decode('UTF-8').split('\n')
+    for line in lines: 
+        if '1\timages/sec' in line:
+            line_split = line.split('\s')
+            start_time = line_split[0].split('|')[1]
+        elif 'total images/sec' in line:
+            line_split = line.split('\s')
+            end_time = line_split[0].split('|')[1]
 
-print(start_time, end_time)
+    print(pod_name, start_time, end_time)
