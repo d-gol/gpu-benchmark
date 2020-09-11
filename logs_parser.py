728f6954b47b2853efc8091135444cc717dfc4b9
Dejan Golubovic <dejan.golubovic.27@gmail.com>
2020-09-11 12:07:55 +0200

    Parser

--- logs_parser.py
+++ logs_parser.py
@@ -2,13 +2,14 @@ import subprocess
 
 res = subprocess.check_output(['kubectl', 'logs', 'dejan-tfjob-resnet50-256-worker-0'])
 
-lines = str(res).split('\\')
+lines = res.decode('UTF-8').split('\n')
 for line in lines:
+    print(line)
     if '1\timages/sec' in line:
         line_split = line.split('\s')
-        start_time = line_split[0].split('|')
+        start_time = line_split[0].split('|')[1]
     elif 'total images/sec' in line:
         line_split = line.split('\s')
-        end_time = line_split[0].split('|')
+        end_time = line_split[0].split('|')[1]
 
 print(start_time, end_time)
